487677b26fdb1ca6bb1704008c33d3ac06949c22
Mphan87 <102190333+Mphan87@users.noreply.github.com>
2022-09-20 15:44:23 -0700

    Add files via upload

---
+++ src/hooks/useFlip.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+
+function useFlip(initialFlipState = true) {
+    const [isFlipped, setFlipped] = useState(initialFlipState);
+  
+    const flip = () => {
+      setFlipped(isUp => !isUp);
+    };
+  
+    return [isFlipped, flip];
+  }
+
+export { useFlip }
